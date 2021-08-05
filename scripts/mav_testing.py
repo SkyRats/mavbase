c67d5d223a4e62ba6aa2ae1829dc87fdca1ba848
brenda <bremsantos@outlook.com>
2021-08-05 09:01:19 -0300

    mudanças dos parametros

--- scripts/mav_testing.py
+++ scripts/mav_testing.py
@@ -3,7 +3,6 @@
 import rospy
 from mavbase.MAV import MAV
 import numpy as np
-TOL = 0.2
 
 def go():
 
@@ -14,8 +13,6 @@ def go():
     goal_x = 5
     goal_y = 5
     altitude = 2
-    goal_lat = mav.gps_target.pose.position.latitude + 0.00005
-    goal_long = mav.gps_target.pose.position.longitude + 0.00005
 
     mav.takeoff(takeoff_alt)
     mav.rate.sleep()
@@ -23,7 +20,7 @@ def go():
     actual_y = mav.drone_pose.pose.position.y
     actual_dist = np.sqrt((goal_x - actual_x)**2 + (goal_y - actual_y)**2)
     rospy.loginfo("Setting position to (%s, %s)" %(str(goal_x), str(goal_y)))
-    while actual_dist > TOL and not rospy.is_shutdown():
+    while actual_dist > 0.2 and not rospy.is_shutdown():
             mav.set_position(goal_x, goal_y, mav.drone_pose.pose.position.z)
             actual_x = mav.drone_pose.pose.position.x
             actual_y = mav.drone_pose.pose.position.y
@@ -33,7 +30,6 @@ def go():
     mav.hold(5)
     rospy.loginfo("Setting altitude to %s m" %(altitude))
     mav.set_altitude(altitude)
-    #mav.go_gps_target(goal_lat, goal_long)
     mav.land()
     mav._disarm()
 
