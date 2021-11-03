fb8c7df997e503cecf48bd7507638fa81fa3e6a0
Igor Pontes Tresolavy <tresolavy@usp.br>
2021-11-03 12:08:47 -0300

    modifies mav_testing.py

--- scripts/mav_testing.py
+++ scripts/mav_testing.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from mavbase.MAV import MAV
 import numpy as np
+import math
 TOL = 0.2
 
 def go():
@@ -11,11 +12,12 @@ def go():
     mav = MAV("1")
 
     takeoff_alt = 10
-    goal_x = 5
-    goal_y = 5
+    t = -math.pi/2
+    goal_x = 4*math.cos(t)
+    goal_y = 4*math.cos(t) * math.sin(t)
     altitude = 2
-    goal_lat = mav.gps_target.pose.position.latitude + 0.00005
-    goal_long = mav.gps_target.pose.position.longitude + 0.00005
+    # goal_lat = mav.gps_target.pose.position.latitude + 0.00005
+    # goal_long = mav.gps_target.pose.position.longitude + 0.00005
 
     mav.takeoff(takeoff_alt)
     mav.rate.sleep()
@@ -23,12 +25,16 @@ def go():
     actual_y = mav.drone_pose.pose.position.y
     actual_dist = np.sqrt((goal_x - actual_x)**2 + (goal_y - actual_y)**2)
     rospy.loginfo("Setting position to (%s, %s)" %(str(goal_x), str(goal_y)))
-    while actual_dist > TOL and not rospy.is_shutdown():
-            mav.set_position(goal_x, goal_y, mav.drone_pose.pose.position.z)
-            actual_x = mav.drone_pose.pose.position.x
-            actual_y = mav.drone_pose.pose.position.y
-            actual_dist = np.sqrt((goal_x - actual_x)**2 + (goal_y - actual_y)**2)
-            mav.rate.sleep()
+    while t < 3*math.pi/2 and not rospy.is_shutdown():
+        mav.set_position(goal_x, goal_y, takeoff_alt)
+        if actual_dist <= TOL:
+            t += 0.2
+            goal_x = 4*math.cos(t)
+            goal_y = 4*math.cos(t) * math.sin(t)
+        actual_x = mav.drone_pose.pose.position.x
+        actual_y = mav.drone_pose.pose.position.y
+        actual_dist = np.sqrt((goal_x - actual_x)**2 + (goal_y - actual_y)**2)
+        mav.rate.sleep()
     rospy.loginfo("On hold")
     mav.hold(5)
     rospy.loginfo("Setting altitude to %s m" %(altitude))
