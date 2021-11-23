854ae813187e0221cbf87090438295746678559c
Igor Pontes Tresolavy <tresolavy@usp.br>
2021-11-23 17:47:30 -0300

    Adds vertical infinity :bento:

--- scripts/infinito_vertical.py
+++ scripts/infinito_vertical.py
@@ -4,7 +4,7 @@ import rospy
 from mavbase.MAV import MAV
 import numpy as np
 import math
-TOL = 0.2
+TOL = 1
 
 def go():
 
@@ -14,7 +14,7 @@ def go():
     takeoff_alt = 5
     t = -math.pi/2
     goal_x = 4*math.cos(t)
-    goal_y = 4*math.cos(t) * math.sin(t)
+    goal_z = 5 + 4*math.cos(t) * math.sin(t)
     # altitude = 2
     # goal_lat = mav.gps_target.pose.position.latitude + 0.00005
     # goal_long = mav.gps_target.pose.position.longitude + 0.00005
@@ -22,18 +22,18 @@ def go():
     mav.takeoff(takeoff_alt)
     mav.rate.sleep()
     actual_x = mav.drone_pose.pose.position.x
-    actual_y = mav.drone_pose.pose.position.y
-    actual_dist = np.sqrt((goal_x - actual_x)**2 + (goal_y - actual_y)**2)
-    rospy.loginfo("Setting position to (%s, %s)" %(str(goal_x), str(goal_y)))
+    actual_z = mav.drone_pose.pose.position.z
+    actual_dist = np.sqrt((goal_x - actual_x)**2 + (goal_z - actual_z)**2)
+    rospy.loginfo("Setting position to (%s, 0, %s)" %(str(goal_x), str(goal_z)))
     while t < 3*math.pi/2 and not rospy.is_shutdown():
-        mav.set_position(goal_x, goal_y, takeoff_alt)
+        mav.set_position(goal_x, 0, goal_z)
         if actual_dist <= TOL:
-            t += 0.2
+            t += 0.1
             goal_x = 4*math.cos(t)
-            goal_y = 4*math.cos(t) * math.sin(t)
+            goal_z = 5 + 4*math.cos(t) * math.sin(t)
         actual_x = mav.drone_pose.pose.position.x
-        actual_y = mav.drone_pose.pose.position.y
-        actual_dist = np.sqrt((goal_x - actual_x)**2 + (goal_y - actual_y)**2)
+        actual_z = mav.drone_pose.pose.position.z
+        actual_dist = np.sqrt((goal_x - actual_x)**2 + (goal_z - actual_z)**2)
         mav.rate.sleep()
     rospy.loginfo("Infinity completed")
     mav.land()
